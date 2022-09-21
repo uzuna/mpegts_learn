1c063227e88a91009631842ac90a5b0354251af4
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-09-21 21:25:00 +0900

    add UAD Dataset attirbute

--- src/uasdms.rs
+++ src/uasdms.rs
@@ -12,6 +12,7 @@ enum Value {
     Timestamp(SystemTime),
     U8(u8),
     U16(u16),
+    U32(u32),
     I16(i16),
     I32(i32),
     String(String),
@@ -19,6 +20,7 @@ enum Value {
 
 #[derive(Debug, PartialEq, Eq)]
 enum UASDataset {
+    Checksum,
     Timestamp,
     LSVersionNumber,
     // Relative between longitudinal axis and True North measured in the horizontal plane.
@@ -41,6 +43,24 @@ enum UASDataset {
     ImageCoordinateSensor,
     SensorLatitude,
     SensorLongtude,
+    SensorTrueAltitude,
+    SensorHorizontalFOV,
+    SensorVerticalFOV,
+    SensorRelativeAzimuthAngle,
+    SensorRelativeElevationAngle,
+    SensorRelativeRollAngle,
+    SlantRange,
+    // ST 0601.8の仕様書ではではu16だがテストデータでは4バイトだったのでu32とする
+    TargetWidth,
+    FrameCenterLatitude,
+    FrameCenterLongitude,
+    FrameCenterElevation,
+    TargetLocationLatitude,
+    TargetLocationLongitude,
+    TargetLocationElevation,
+    // Meters/Second
+    PlatformGroundSpeed,
+    GroundRange,
 }
 
 impl DataSet for UASDataset {
@@ -50,16 +70,34 @@ impl DataSet for UASDataset {
     where
         Self: std::marker::Sized,
     {
+        use UASDataset::*;
         match b {
-            2 => Some(UASDataset::Timestamp),
-            5 => Some(UASDataset::PlatformHeadingAngle),
-            6 => Some(UASDataset::PlatformPitchAngle),
-            7 => Some(UASDataset::PlatformRollAngle),
-            11 => Some(UASDataset::ImageSourceSensor),
-            12 => Some(UASDataset::ImageCoordinateSensor),
-            13 => Some(UASDataset::SensorLatitude),
-            14 => Some(UASDataset::SensorLongtude),
-            65 => Some(UASDataset::LSVersionNumber),
+            1 => Some(Checksum),
+            2 => Some(Timestamp),
+            5 => Some(PlatformHeadingAngle),
+            6 => Some(PlatformPitchAngle),
+            7 => Some(PlatformRollAngle),
+            11 => Some(ImageSourceSensor),
+            12 => Some(ImageCoordinateSensor),
+            13 => Some(SensorLatitude),
+            14 => Some(SensorLongtude),
+            15 => Some(SensorTrueAltitude),
+            16 => Some(SensorHorizontalFOV),
+            17 => Some(SensorVerticalFOV),
+            18 => Some(SensorRelativeAzimuthAngle),
+            19 => Some(SensorRelativeElevationAngle),
+            20 => Some(SensorRelativeRollAngle),
+            21 => Some(SlantRange),
+            22 => Some(TargetWidth),
+            23 => Some(FrameCenterLatitude),
+            24 => Some(FrameCenterLongitude),
+            25 => Some(FrameCenterElevation),
+            40 => Some(TargetLocationLatitude),
+            41 => Some(TargetLocationLongitude),
+            42 => Some(TargetLocationElevation),
+            56 => Some(PlatformGroundSpeed),
+            57 => Some(GroundRange),
+            65 => Some(LSVersionNumber),
             _ => None,
         }
     }
@@ -75,8 +113,14 @@ impl DataSet for UASDataset {
                     None => Err(ParseError::ValueError("failed to parse timestamp.".into())),
                 }
             }
-            // TODO Change value to Degrees
-            PlatformHeadingAngle => {
+            PlatformGroundSpeed => Ok(Value::U8(v[0])),
+            Checksum
+            | PlatformHeadingAngle
+            | SensorTrueAltitude
+            | SensorHorizontalFOV
+            | SensorVerticalFOV
+            | FrameCenterElevation
+            | TargetLocationElevation => {
                 let mut rdr = Cursor::new(v);
                 let angle = rdr.read_u16::<BigEndian>().unwrap();
                 Ok(Value::U16(angle))
@@ -86,10 +130,22 @@ impl DataSet for UASDataset {
                 let angle = rdr.read_i16::<BigEndian>().unwrap();
                 Ok(Value::I16(angle))
             }
-            SensorLatitude | SensorLongtude => {
+            SensorLatitude
+            | SensorLongtude
+            | SensorRelativeElevationAngle
+            | SensorRelativeRollAngle
+            | FrameCenterLatitude
+            | FrameCenterLongitude
+            | TargetLocationLatitude
+            | TargetLocationLongitude => {
                 let mut rdr = Cursor::new(v);
                 let angle = rdr.read_i32::<BigEndian>().unwrap();
                 Ok(Value::I32(angle))
+            }
+            SensorRelativeAzimuthAngle | SlantRange | TargetWidth | GroundRange => {
+                let mut rdr = Cursor::new(v);
+                let angle = rdr.read_u32::<BigEndian>().unwrap();
+                Ok(Value::U32(angle))
             }
             ImageSourceSensor | ImageCoordinateSensor => {
                 Ok(Value::String(String::from_utf8(v.to_owned()).unwrap()))
@@ -119,6 +175,23 @@ mod tests {
             12, 14, 0x47, 0x65, 0x6f, 0x64, 0x65, 0x74, 0x69, 0x63, 0x20, 0x57, 0x47, 0x53, 0x38, 0x34,
             13, 4, 0x4d, 0xc4, 0xdc, 0xbb,
             14, 4, 0xb1, 0xa8, 0x6c, 0xfe,
+            15, 2, 0x1f, 0x4a,
+            16, 2, 0x00, 0x85,
+            17, 2, 0x00, 0x4b,
+            18, 4, 0x20, 0xc8, 0xd2, 0x7d,
+            19, 4, 0xfc, 0xdd, 0x02, 0xd8,
+            20, 4, 0xfe, 0xb8, 0xcb, 0x61,
+            21, 4, 0x00, 0x8f, 0x3e, 0x61,
+            22, 4, 0x00, 0x00, 0x01, 0xc9,
+            23, 4, 0x4d, 0xdd, 0x8c, 0x2a,
+            24, 4, 0xb1, 0xbe, 0x9e, 0xf4,
+            25, 2, 0x0b, 0x85,
+            40, 4, 0x4d, 0xdd, 0x8c, 0x2a,
+            41, 4, 0xb1, 0xbe, 0x9e, 0xf4,
+            42, 2, 0x0b, 0x85,
+            56, 1, 0x2e,
+            57, 4, 0x00, 0x8d, 0xd4, 0x29,
+            1, 2, 0x1c, 0x5f,
             ];
 
         let klv = KLVReader::<UASDataset>::from_bytes(&buf);
