244c0243d948fd81373ea55d854ab83d3d496eec
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-09-21 11:28:43 +0900

    add KLV struct

--- src/klv.rs
+++ src/klv.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, marker::PhantomData};
 
 pub struct KLVRaw<'buf>(&'buf [u8]);
 
@@ -55,14 +55,42 @@ impl<'buf> Iterator for KLVRawReader<'buf> {
     }
 }
 
-// pub struct KLV<'buf, T> {
-//     buf: &'buf [u8],
-//     _phantom: PhantomData<T>
-// }
+trait DataSet {
+    type Item;
+    fn from_byte(b: u8) -> Option<Self>
+    where
+        Self: std::marker::Sized;
+    fn value(&self, v: &[u8]) -> Self::Item;
+}
+
+struct KLV<'buf, K> {
+    buf: &'buf [u8],
+    _phantom: PhantomData<K>,
+}
+
+impl<'buf, K: DataSet> KLV<'buf, K> {
+    pub fn from_bytes(buf: &'buf [u8]) -> Self {
+        Self {
+            buf,
+            _phantom: PhantomData,
+        }
+    }
+    pub fn key(&self) -> Option<K> {
+        K::from_byte(self.buf[0])
+    }
+    #[inline]
+    fn len(&self) -> usize {
+        self.buf[1] as usize
+    }
+    pub fn value(&self) -> Option<K::Item> {
+        self.key()
+            .map(|key| key.value(&self.buf[2..2 + self.len()]))
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::KLVRawReader;
+    use super::{DataSet, KLVRawReader, KLV};
 
     #[test]
     fn test_iterator() {
@@ -71,5 +99,40 @@ mod tests {
         for v in r {
             println!("{:?}", v);
         }
+    }
+
+    #[derive(Debug)]
+    enum DummyValue {
+        U8(u8),
+    }
+
+    #[derive(Debug)]
+    enum DummyDataset {
+        One,
+        Two,
+    }
+    impl DataSet for DummyDataset {
+        type Item = DummyValue;
+        fn from_byte(b: u8) -> Option<Self> {
+            match b {
+                1 => Some(Self::One),
+                2 => Some(Self::Two),
+                _ => None,
+            }
+        }
+        fn value(&self, v: &[u8]) -> Self::Item {
+            match self {
+                DummyDataset::One => DummyValue::U8(v[0]),
+                DummyDataset::Two => DummyValue::U8(v[0]),
+            }
+        }
+    }
+
+    #[test]
+    fn test_klv() {
+        let buf = vec![1, 1, 0];
+        let v = KLV::<DummyDataset>::from_bytes(&buf);
+
+        println!("debug {:?} {:?}", v.key(), v.value());
     }
 }
