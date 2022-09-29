ce3e5e1372e2f12ec3f34eed6f7308b8207c4357
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-09-29 22:04:38 +0900

    add yaml generator

---
+++ cisupport/src/main.rs
@@ -0,0 +1,21 @@
+use std::str::FromStr;
+fn main() {
+    let src_dir = "workflows";
+    let dest_dir = "../.github/workflows";
+
+    for x in std::fs::read_dir(src_dir).unwrap() {
+        let src_path = x.unwrap().path();
+        println!("{}", src_path.display());
+        let content = std::fs::read_to_string(&src_path).unwrap();
+        let docs = yaml_rust::YamlLoader::load_from_str(&content).unwrap();
+        println!("{:?}", &docs[0]);
+        let mut out_str = String::new();
+        {
+            let mut emitter = yaml_rust::YamlEmitter::new(&mut out_str);
+            emitter.dump(&docs[0]).unwrap(); // dump the YAML object to a String
+        }
+        let dest_dir = std::path::PathBuf::from_str(dest_dir).unwrap();
+        let destfile = dest_dir.join(src_path.file_name().unwrap());
+        std::fs::write(&destfile, &out_str).unwrap();
+    }
+}
